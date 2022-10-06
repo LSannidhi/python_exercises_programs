d80b4dd1b109020ff7761ba30d521fab7bf2ed29
Lakshmi <111857232+LSannidhi@users.noreply.github.com>
2022-10-06 20:34:03 +1100

    Add files via upload

---
+++ ls_arithmetic_own.py
@@ -0,0 +1,19 @@
+# name=input("What is the name of your project? ")
+# print("Hello"+ name )
+
+a=input("Please enter your first value:")
+b=input("Please enter your second value:")
+c=float(a)+float(b)
+d=float(a)-float(b)
+e=float(b)-float(a)
+f=float(a)*float(b)
+g=float(a)/float(b)
+# 
+
+result=print(c)
+result=print(d)
+result=print(e)
+result=print(f)
+result=print(g)
+
+
