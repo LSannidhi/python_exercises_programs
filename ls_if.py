8e222d84c7eb2f3b6195ac94be3178c7c79e4c11
Lakshmi <111857232+LSannidhi@users.noreply.github.com>
2022-10-16 22:11:45 +1100

    Add files via upload

---
+++ ls_if.py
@@ -0,0 +1,3 @@
+a=int(input("please enter a number: "))
+if a%2==0:
+print("a is even")
---
+++ ls_own_capitalize.py
@@ -0,0 +1,12 @@
+from string import ascii_letters, ascii_lowercase, digits, punctuation
+
+a=input("input Alphanumeric characters: ")
+b=a[0]
+# if b==ascii_lowercase:
+b=alphabets=[]
+for b in range(65,91):
+ c=a.capitalize()
+ print(c)
+ if b==digits or punctuation:
+  break
+ print(a)
